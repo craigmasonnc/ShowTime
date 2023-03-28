7a7d994781e2603cef8ce5c8137190e811359cb5
craigmasonnc <craigmasonnc@gmail.com>
2023-03-28 10:24:10 -0400

    continuous clock

--- ShowTime.ps1
+++ ShowTime.ps1
@@ -1,5 +1,9 @@
-$dateString = Get-Date
 
-Write-Ascii -InputObject $dateString
 
-Read-Host -Prompt "Press Enter to exit"
+do{
+    Clear-Host
+    $dateString = Get-Date
+    Write-Ascii -InputObject $dateString
+    Start-Sleep -seconds 1
+}until ([System.Console]::KeyAvailable)
+
